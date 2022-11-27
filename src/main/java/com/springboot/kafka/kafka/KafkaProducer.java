dc34578f21cbbbb2d2c7da763279a453ec1ee3f3
akamsi <sax.747@gmail.com>
2022-11-27 18:29:26 +0200

    Add producer

---
+++ src/main/java/com/springboot/kafka/kafka/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.springboot.kafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private static final String TOPIC = "topic1";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(final String message) {
+        kafkaTemplate.send(TOPIC, message);
+        LOGGER.info(String.format("Message sent: %s", message));
+    }
+}
